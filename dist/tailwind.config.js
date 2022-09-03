03e26dfde298db3a26308ec53f4bf32bf34d3575
varshapun <vdhinglani@gmail.com>
2022-09-03 18:19:39 +0530

    initial commit

---
+++ dist/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkGreyishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryLightGrey: 'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+}
